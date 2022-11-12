84a209f084dc0b13a4192818efcd1306849268e9
Max Bakulin <mbakulin102@gmail.com>
2022-11-12 23:21:36 +0300

    added code implementation

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿string[] arrayOne = new string[5] {"123", "23", "hello", "world", "res"};
+string[] arrayTwo = new string[arrayOne.Length];
+void GetArrayWithDesiredStrings(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+GetArrayWithDesiredStrings(arrayOne, arrayTwo);
+PrintArray(arrayTwo);
